27979eda8166602469d1099cc687454cf3fb9573
Krokodille777 <krokocoder123@gmail.com>
2024-10-06 12:05:52 +0300

    Big Bug Fixed. New sum (ss) and division by 2 (div) are added

--- 1/project.cpp
+++ 1/project.cpp
@@ -2,27 +2,39 @@
 using namespace std;
 
 int main(){
-    int N, n =1,s = 0, se = 0, so = 0;
-    cout<<"Enter N: ";
-    cin>>N;
-    if (N<1)
-        cout<<"N must be greater than 1";
-    else
-        while(n<=N){
-            s+=n;
-            n++;
-            if(n%2==0){
-                se+=n;
-            }else if (n%2==1){
-                so+=n;
+    int N, n = 1, s = 0, se = 0, so = 0;
+    int ss = 0;
+    double div;
+    
+    cout << "Enter N: ";
+    cin >> N;
+    
+    if (N < 1) {
+        cout << "N must be greater than or equal to 1";
+    } else {
+        while (n <= N) {
+            s += n;  // Sum of all numbers
+            
+            // Check if n is even or odd and add accordingly
+            if (n % 2 == 0) {
+                se += n;  // Even sum
+            } else {
+                so += n;  // Odd sum
             }
-            else
-                n++;
+            
+            n++;  // Increment the counter
         }
         
-    cout<<"Sum = "<<s;
-    cout<<"\nSum of even numbers = "<<se;
-    cout<<"\nSum of odd numbers = "<<so;
-    return 0;
+        // Output the results
+        cout << "Sum of all numbers = " << s;
+        cout << "\nSum of even numbers = " << se;
+        cout << "\nSum of odd numbers = " << so;
 
+
+        ss = s + se + so;
+        div = ss/2;
+        cout<<"ss ="<<ss<<"\t div = "<<div;
+    }
+
+    return 0;
 }
