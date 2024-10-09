7d6b48810622d2eff4e42737388e9d4e6f0249ae
Krokodille777 <krokocoder123@gmail.com>
2024-10-09 15:26:08 +0300

    Final task completed!

---
+++ 5/address.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cout<<"Enter a: ";
+    cin>>a;
+    cout<<"Enter b: ";
+    cin>>b;
+    for(int i = a ; i <= b; i++){
+        cout<<&a<<endl;
+        cout<<&b<<endl; break;
+        cout<<-i<<endl;
+
+}
+    return 0;
+}
